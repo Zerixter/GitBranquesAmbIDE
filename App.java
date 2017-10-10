f5678cb86e5bbeec612ce010b01b954a144b1a83
Hamza Saddouki <hsaddouki@gmail.com>
2017-10-10 19:28:06 +0200

    Modificat App.java

--- App.java
+++ App.java
@@ -1,14 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
-    private static final int LLISTA[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    private static final int NUMERO_A_MIRAR = 5;
-    
+    private static final int LONGITURA_LLISTA = 1000000;
+    private static final Random RANDOM = new Random();
+    private static final Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        for (int numero: LLISTA) {
-            if (numero == NUMERO_A_MIRAR) {
-                System.out.println("El numero es troba en el array");
+        List<Integer> llista = new ArrayList<>();
+
+        for (int i = 0;i < LONGITURA_LLISTA;i++) {
+            llista.add(RANDOM.nextInt(1000000));
+        }
+        
+        do {
+            System.out.println("Quin numero vols revisar que estigui en el array?");
+        }while (!in.hasNextInt());
+        int numero_a_mirar = in.nextInt();
+
+        long temps_abans_de_for = System.nanoTime();
+
+        boolean esta_en_el_array = false;
+        for (int numero: llista) {
+            if (numero == numero_a_mirar) {
+                esta_en_el_array = true;
                 break;
             }
         }
+        long temps_despres_de_for = System.nanoTime();
+        long nano_segons_passats = temps_despres_de_for - temps_abans_de_for;
+
+        if (esta_en_el_array) {
+            System.out.println("El numero "+numero_a_mirar+" està en el array");
+        } else {
+            System.out.println("El numero "+numero_a_mirar+" no està en el array");
+        }
+
+        System.out.println("Han passat "+nano_segons_passats+" nanosegons");
     }
 }
